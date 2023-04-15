b7097f496a31687d47e11653b35bc31f2a04a421
Thanushree <thanushree@swayaan.com>
2023-04-15 13:11:35 +0530

    Initial commit

---
+++ src/test/java/com/example/samplemavenproject/SampleMavenProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samplemavenproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleMavenProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
